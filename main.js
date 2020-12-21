b45e3e0991c2fb41f3ee5eeccab333221f864938
Zymantasdan <z.dansevicius@gmail.com>
2020-12-21 21:21:46 +0200

    ipuseta

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-// Open the full screen search box
-
 function openSearch() {
-    document.getElementById("myOverlay").style.display = "block";
-  }
-  // Close the full screen search box
-  function closeSearch() {
-    document.getElementById("myOverlay").style.display = "none";
-  }
-  
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
